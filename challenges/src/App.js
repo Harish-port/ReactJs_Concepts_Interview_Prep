ba598c3a6b1332590f281bf5a66c756511e4c4b3
Harish S <harish.sq@gmail.com>
2024-12-23 18:23:52 +0530

    Pagination machine coding react added

--- challenges/src/App.js
+++ challenges/src/App.js
@@ -1,11 +1,13 @@
 import { useState } from "react";
 import TodoWrapper from "./Challenges/Todo/TodoWrapper";
 import Timer from "./Challenges/stopwatch/Timer";
+import FrontEndPagination from "./Challenges/Pagination/FrontendFocusedPagination";
+import BackendEndPagination from "./Challenges/Pagination/BackendFocusedPagiantion";
 function App() {
   const [todos,addTodos] =useState('')
   return (
     <div className="App">
-      <Timer />
+      <BackendEndPagination />
     </div>
   );
 }
---
+++ challenges/src/Challenges/Pagination/BackendFocusedPagiantion.js
@@ -0,0 +1,79 @@
+import { useEffect, useState } from "react";
+import "./pagination.css";
+const BackendEndPagination = () => {
+  const [products, setProducts] = useState([]);
+  const [page, setPage] = useState(1);
+  const [totalPages, setTotalPages] = useState(0);
+
+  const fetchData = async () => {
+    const res = await fetch(
+      `https://dummyjson.com/products?limit=10&skip=${page * 10 - 10}`
+    );
+    const data = await res.json();
+    if (data && data.products) {
+      setProducts(data.products);
+      setTotalPages(Math.floor(data.total / 20));
+    }
+    console.log(data);
+  };
+  useEffect(() => {
+    fetchData();
+  }, [page]);
+  console.log(products, "products<<<");
+  const selectPageHandler = (selectedPage) => {
+    if (
+      selectedPage >= 1 &&
+      selectedPage <= totalPages &&
+      selectedPage !== page
+    )
+      setPage(selectedPage);
+  };
+  console.log(totalPages,"totalPages");
+  
+  return (
+    <>
+      {products?.length > 0 && (
+        <div className="products">
+          {products.map((product) => {
+            return (
+              <span className="products__single" key={product.id}>
+                <img src={product.thumbnail} alt={product.title} />
+                <span>{product.title}</span>
+              </span>
+            );
+          })}
+        </div>
+      )}
+      {products?.length > 0 && (
+        <div className="pagination">
+          <span
+            className={page > 1 ? "" : "pagination__disable"}
+            onClick={() => selectPageHandler(page - 1)}
+          >
+            Previos
+          </span>
+          {[...Array(totalPages)].map((_, i) => {
+            return (
+              <span
+                className={page === i + 1 ? "pagination__selected" : ""}
+                onClick={() => selectPageHandler(i + 1)}
+                key={i}
+              >
+                {i + 1}
+              </span>
+            );
+          })}
+          <span
+            className={page < totalPages ? " " : "pagination__disable"}
+            onClick={() => selectPageHandler(page + 1)}
+          >
+            {" "}
+            Next{" "}
+          </span>
+        </div>
+      )}
+    </>
+  );
+};
+
+export default BackendEndPagination;
